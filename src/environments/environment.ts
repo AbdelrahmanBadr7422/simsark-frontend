01aa1c0f62afaf8ae1a58d4a04f15d3e2816af68
Terror-1 <elfarhosam487@gmail.com>
2025-10-20 11:05:53 +0300

    update vercel.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000/api/v1',
+  apiBaseUrl: 'https://simsark.vercel.app/api/v1',
   stripe: {
     publicKey: 'sk_test_BQokikJOvBiI2HlWgH4olfQ2',
   },
