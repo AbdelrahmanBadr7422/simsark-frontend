2ad1dae5c50f4bc75dfa07ed22a073931e5c3668
osamaelsaeed <om35453@gmail.com>
2025-10-21 13:44:50 +0300

    navbar enhancment

--- src/app/components/shared/navbar/navbar.ts
+++ src/app/components/shared/navbar/navbar.ts
@@ -1,24 +1,35 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { Auth } from '../../../services/auth';
 import { AsyncPipe, CommonModule, NgIf } from '@angular/common';
+
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterLink, RouterLinkActive, AsyncPipe, CommonModule, NgIf],
   templateUrl: './navbar.html',
 })
 export class Navbar {
   private authService = inject(Auth);
   private router = inject(Router);
+
   isLogged$ = this.authService.isLogged$;
   isSeller$ = this.authService.isSeller$;
+
   menuOpen = false;
+  dropdownOpen = false; // ✅ Add this
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen; // ✅ Add this
+  }
+
   logout() {
     this.authService.logout();
+    this.dropdownOpen = false; // optional — close dropdown on logout
     this.router.navigate(['/home']);
   }
 }
