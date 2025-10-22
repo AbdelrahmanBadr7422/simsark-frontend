83de50e783732c3ab1fe3a053123833bd8b03193
osamaelsaeed <om35453@gmail.com>
2025-10-22 05:07:10 +0300

    user page

--- src/app/components/auth/user/user.ts
+++ src/app/components/auth/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,6 +6,32 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './user.html',
 })
-export class User {
+export class User implements OnInit {
+  sidebarOpen = false;
+  isDesktop = false;
   isSeller = localStorage.getItem('userRole') === 'seller';
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', [])
+  checkScreenSize() {
+    this.isDesktop = window.innerWidth >= 768;
+    if (this.isDesktop) {
+      this.sidebarOpen = true;
+    } else {
+      this.sidebarOpen = false;
+    }
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  closeSidebar() {
+    if (!this.isDesktop) {
+      this.sidebarOpen = false;
+    }
+  }
 }
